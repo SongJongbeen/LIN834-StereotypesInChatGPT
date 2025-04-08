d7fafd64c6a1a3cd099303b59e09e80433b77b64
SongJongbeen <1041489@gmail.com>
2025-04-08 15:01:18 +0900

    update model.py

--- model.py
+++ model.py
@@ -9,7 +9,7 @@ load_dotenv()
 client = OpenAI(api_key=os.getenv("OPENAI_API"))
 
 def load_prompt_template():
-    # YAML 파일에서 프롬프트 템플릿 로드
+    # load prompt template from yaml file
     with open("data/base_prompts.yaml", "r", encoding="utf-8") as f:
         prompt_template = yaml.safe_load(f)
     return prompt_template
@@ -17,11 +17,12 @@ def load_prompt_template():
 def get_categories():
     # read json
     with open("data/categories.json", "r", encoding="utf-8") as f:
-        categories = json.load(f)
-    return categories
+        data = json.load(f)
+    return data
 
 categories = get_categories()
 politicals = categories["political"]
+genders = categories["gender"]
 
 def ask():
     for category in categories:
@@ -30,6 +31,7 @@ def ask():
 
         for value in categories[category]:
             print(f"processing {category}/{value}")
+
             prompt = load_prompt_template()
             response = client.chat.completions.create(
                 model="gpt-4o",
